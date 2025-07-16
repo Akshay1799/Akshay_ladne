5a8b5f4d16a02587c8aef18f0a3fef0988b8fe0f
Akshay Ladne <Kumawata283@gmail.com>
2025-07-16 23:18:51 +0530

    initial commit

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar.jsx"; // Import the Navbar component
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Portfolio/src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../index.css';
+const Navbar = () => {
+  return (
+    <div>
+        <h1 className='text-red-600'>Hey Akshay! 👋</h1>
+    </div>
+  )
+}
+
+export default Navbar
